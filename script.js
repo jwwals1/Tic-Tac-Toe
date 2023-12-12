48c8afecf653e170671dec01729e24caba440108
jwwals1 <joe_walsh59@yahoo.com>
2023-12-12 17:42:44 -0600

    added playerMove function

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 const GameBoard = (() => {
     let gameBoard = ['', '', '', '', '', '', '', '', ''];
-    console.log(gameBoard)
+
+    const player = (name, symbol, turn) => {
+        return { name, symbol, turn };
+    };
+
+    const player1 = player('player 1', "X", true);
+    const player2 = player('player 2', "O", false);
+
+    const playerMove = (function () {
+        const square = document.querySelectorAll('.square');
+        square.forEach(square => {
+            square.addEventListener('click', event => {
+                if (player1.turn == true && event.target.textContent == '') {
+                    square.textContent = player1.symbol
+                    player1.turn = false
+                    player2.turn = true
+                }
+                else if (player2.turn == true && event.target.textContent == '') {
+                    square.textContent = player2.symbol
+                    player2.turn = false
+                    player1.turn = true
+                }
+            })
+        })
+    })
+
 
     const render = () => {
         let boardHTML = '';
@@ -10,13 +35,14 @@ const GameBoard = (() => {
         document.querySelector("#gameboard").innerHTML = boardHTML;
     }
     return {
-        render
+        render, playerMove
     }
+
 })();
 
-// const Player1 = {
-//     player1: 'player1'
-// }
+const Player1 = (() => {
+
+})();
 
 // const Player2 = {
 //     player2: 'player2'
