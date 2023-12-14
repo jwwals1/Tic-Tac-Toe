0ec81defa81db64a8c7bd4f3b41643efebeb09c6
jwwals1 <joe_walsh59@yahoo.com>
2023-12-14 11:47:28 -0600

    added checkForWinner function

--- script.js
+++ script.js
@@ -1,53 +1,96 @@
 const GameBoard = (() => {
-    let gameBoard = ['', '', '', '', '', '', '', '', ''];
-
-    const player = (name, symbol, turn) => {
-        return { name, symbol, turn };
-    };
-
-    const player1 = player('player 1', "X", true);
-    const player2 = player('player 2', "O", false);
-
-    const playerMove = (function () {
-        const square = document.querySelectorAll('.square');
-        square.forEach(square => {
-            square.addEventListener('click', event => {
-                if (player1.turn == true && event.target.textContent == '') {
-                    square.textContent = player1.symbol
-                    player1.turn = false
-                    player2.turn = true
-                }
-                else if (player2.turn == true && event.target.textContent == '') {
-                    square.textContent = player2.symbol
-                    player2.turn = false
-                    player1.turn = true
-                }
-            })
-        })
-    })
-
-
+    let gameBoard = ['', '', '', '', '', '', '', '', '']
     const render = () => {
         let boardHTML = '';
         gameBoard.forEach((square, index) => {
-            boardHTML += `<div class="square" id=square-${index}">${square}</div>`
+            boardHTML += `<div class="square" id="square-${index}">${square}</div>`
         });
         document.querySelector("#gameboard").innerHTML = boardHTML;
+        const squares = document.querySelectorAll(".square");
+        squares.forEach((square) => {
+            square.addEventListener("click", Game.handleClick)
+        })
+    }
+    const update = (index, value) => {
+        gameBoard[index] = value
+        render();
+    }
+
+    const getGameboard = () => gameBoard
+
+    return {
+        render, update, getGameboard
+    }
+})()
+
+const createPlayer = (name, symbol) => {
+    return {
+        name,
+        symbol,
+    }
+}
+
+const Game = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector('#player1').value, "X"),
+            createPlayer(document.querySelector('#player2').value, "O")
+        ]
+        currentPlayerIndex = 0;
+        gameOver = false
+        GameBoard.render();
+        const squares = document.querySelectorAll(".square");
+        squares.forEach((square) => {
+            square.addEventListener("click", handleClick)
+        })
+    }
+    const handleClick = (event) => {
+        let index = parseInt(event.target.id.split("-")[1]);
+
+        if (GameBoard.getGameboard()[index] !== '')
+            return;
+        GameBoard.update(index, players[currentPlayerIndex].symbol);
+
+        if (checkForWinner(GameBoard.getGameboard(), players[currentPlayerIndex].symbol)) {
+            gameOver = true
+            alert("winner")
+        }
+
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0
     }
     return {
-        render, playerMove
+        start, handleClick
     }
-
 })();
 
-const Player1 = (() => {
 
-})();
+function checkForWinner(board) {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
 
-// const Player2 = {
-//     player2: 'player2'
-// }
+    for (let i = 0; i < winningCombinations.length; i++) {
+        const [a, b, c] = winningCombinations[i];
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            console.log('winner')
+            return true
+        }
+    }
+    return false
+}
 
-// const GameFlow {
-
-// }
+const startButton = document.querySelector('#start-button');
+startButton.addEventListener("click", () => {
+    Game.start()
+})
